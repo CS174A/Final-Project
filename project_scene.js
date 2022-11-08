006ab88b99dd1b68f2cb93158bb59408fd812bbb
Sanya <purvi.sanya@gmail.com>
2022-11-08 14:28:51 -0800

    check if airplane leaves viewport + fix translation logic

--- project_scene.js
+++ project_scene.js
@@ -34,18 +34,26 @@ export class Project_Scene extends Scene {
         this.airplane_model_transform = Mat4.identity();
 
         // *** Control Panel toggles
-        this.f_pressed = 0;
+        this.fly_higher = 0;
+        this.log_to_console = 0;
     }
 
     make_control_panel() {
         // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
         this.key_triggered_button("Fly higher", ["f"], () => {
-            this.f_pressed ^= 1;
+            this.fly_higher ^= 1;
+        });
+        this.key_triggered_button("Log to console", ["l"], () => {
+            this.log_to_console ^= 1;
         });
     }
 
+    game_over() {
+
+    }
+
     display(context, program_state) {
-        // *** Setup
+        // *** Setup *** //
 
         // display():  Called once per frame of animation.
         // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
@@ -65,35 +73,62 @@ export class Project_Scene extends Scene {
         const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
         const yellow = hex_color("#fac91a");
 
-        // *** Draw below
+        // *** Draw below *** //
 
-        // TODO: Draw airplane.
+        // *** TODO: Draw airplane.
         // (Maybe shadows later)
         this.shapes.torus.draw(context, program_state, this.airplane_model_transform, this.materials.test.override({color: yellow}));
 
-        // TODO: Draw 3D landscape.
+        // *** TODO: Draw 3D landscape.
 
-        // Keep moving camera up and to the right.
+        // Keep moving camera sideways.
         let new_camera_location = Mat4.inverse(this.initial_camera_location);
         new_camera_location = new_camera_location
-            .times(Mat4.translation(t, t/3, 0));
+            .times(Mat4.translation(t, 0, 0)); // this is always a brand new translation by t.
         new_camera_location = Mat4.inverse(new_camera_location);
         program_state.set_camera(new_camera_location);
 
-        // Control the airplane.
-        if (this.f_pressed) {
+        // *** Control the airplane.
+        let previous_translation_x =  this.airplane_model_transform[0][3]; // t is applied on top of an existing, cumulative translation of ex t.
+        if (this.fly_higher) {
             this.airplane_model_transform = this.airplane_model_transform
-                .times(Mat4.translation(0.1, 0.8, 0));
-            this.f_pressed ^= 1;
+                .times(Mat4.translation(t - previous_translation_x, 0.8, 0));
+            this.fly_higher ^= 1;
         } else {
             this.airplane_model_transform = this.airplane_model_transform
-                .times(Mat4.translation(0.014, - 0.02, 0)); // when it's a function of time it goes down faster as time goes on. 0.015 is straight down wrt t. < 0.015 is left, > is right.
+                .times(Mat4.translation(t - previous_translation_x, - 0.04, 0))
         }
 
-        // TODO: Draw persisting clouds that drift left per frame.
+        // *** TODO: Draw persisting clouds that drift left per frame.
         // (Also power ups later)
 
-        // TODO: Detect collisions with clouds and top and bottom of screen.
+        // *** TODO: Detect collisions with clouds and top and bottom of screen.
+        //  https://learnopengl.com/In-Practice/2D-Game/Collisions/Collision-detection
+        let size = 3; // Adjust after implementing airplane.
+
+        // Get max left, right, top, bottom positions of airplane := model_transform ± size
+        let left_airplane = this.airplane_model_transform[0][3] + size;
+        let right_airplane = this.airplane_model_transform[0][3] - size;
+        let top_airplane = this.airplane_model_transform[1][3] + size;
+        let bottom_airplane = this.airplane_model_transform[1][3] - size;
+
+        // Check if airplane leaves viewport.
+        if (top_airplane >= 10.5) {
+            console.log("Game over. Player touched the top.")
+        } else if (bottom_airplane <= -15) {
+            console.log("Game over. Player touched the bottom.")
+        }
+
+
+        // plane.right > cloud.left && cloud.right > plane.left
+        // plane.top > cloud.bottom && cloud.top > plane.bottom
+
+
+        if (this.log_to_console) {
+            console.log("top_airplane", top_airplane)
+            console.log("bottom_airplane", bottom_airplane)
+            this.log_to_console ^= 1;
+        }
 
     }
 }
